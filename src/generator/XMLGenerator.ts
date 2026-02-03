1d432356e8f7b8e60eb39d6526d8d2966c9a2640
hjx <hjx2019@gmail.com>
2026-02-03 13:19:45 +0800

    feat: Task 4 - Implement XMLGenerator for standard FGUI XML output

---
+++ src/generator/XMLGenerator.ts
@@ -0,0 +1,89 @@
+import * as xmlbuilder from 'xmlbuilder';
+import { UINode, ResourceInfo } from '../models/UINode';
+import { ObjectType } from '../models/FGUIEnum';
+import { PropertyMapper } from '../mapper/PropertyMapper';
+
+/**
+ * XMLGenerator: Responsible for producing valid FGUI XML files.
+ */
+export class XMLGenerator {
+    private _mapper = new PropertyMapper();
+
+    /**
+     * Generates component XML from a list of UI nodes.
+     */
+    public generateComponentXml(nodes: UINode[], buildId: string): string {
+        const component = xmlbuilder.create('component').att('size', '1440,1024');
+        const displayList = component.ele('displayList');
+
+        nodes.forEach(node => {
+            const attributes = this._mapper.mapAttributes(node);
+            let eleName = 'graph';
+
+            switch (node.type) {
+                case ObjectType.Text:
+                    eleName = 'text';
+                    break;
+                case ObjectType.Image:
+                case ObjectType.Loader:
+                    eleName = 'loader';
+                    if (node.src) {
+                        // Resource reference logic
+                        const resId = this.findResourceIdBySrc(node.src);
+                        if (resId) {
+                            attributes.url = `ui://${buildId}${resId}`;
+                        }
+                    }
+                    break;
+                case ObjectType.Button:
+                case ObjectType.Component:
+                    eleName = 'component';
+                    break;
+                case ObjectType.InputText:
+                    eleName = 'text'; // FGUI uses <text> with specific flags for input
+                    attributes.input = "true";
+                    break;
+            }
+
+            const element = displayList.ele(eleName, attributes);
+
+            // Special sub-elements
+            if (node.type === ObjectType.Button) {
+                element.att('extention', 'Button');
+                element.ele('Button', { title: node.text || "" });
+            }
+        });
+
+        return component.end({ pretty: true });
+    }
+
+    /**
+     * Generates package.xml description.
+     */
+    public generatePackageXml(resources: ResourceInfo[], buildId: string, packName: string): string {
+        const pkgDesc = xmlbuilder.create('packageDescription').att('id', buildId);
+        const resNode = pkgDesc.ele('resources');
+
+        // Main component is always present
+        resNode.ele('component', { id: 'main_id', name: 'main.xml', path: '/', exported: 'true' });
+
+        resources.forEach(res => {
+            resNode.ele(res.type, { 
+                id: res.id, 
+                name: res.name, 
+                path: res.type === 'image' ? '/img/' : '/', 
+                exported: 'true' 
+            });
+        });
+
+        const publish = pkgDesc.ele('publish', { name: packName });
+        publish.ele('atlas', { name: 'Default', index: 0 });
+
+        return pkgDesc.end({ pretty: true });
+    }
+
+    private findResourceIdBySrc(src: string): string | null {
+        // This is a placeholder; actual mapping is handled in UIPackage integration phase
+        return null; 
+    }
+}
